ddc3188f1dc0a327c57d85c789f54323402c4641
edgabrielyan01 <edgar.gabrielyan.tw@gmail.com>
2025-05-26 21:33:31 +0400

    add Singleton pattern

--- Creational_Patterns/Singleton.php
+++ Creational_Patterns/Singleton.php
@@ -1,1 +1,29 @@
 <?php
+
+class Singleton
+{
+    private static ?Singleton $instance = null;
+    private function __construct() {}
+
+    public static function getInstance(): Singleton
+    {
+        if (self::$instance === null) {
+            self::$instance = new self();
+        }
+
+        return self::$instance;
+    }
+}
+
+function clientCode(): void
+{
+    $s1 = Singleton::getInstance();
+    $s2 = Singleton::getInstance();
+    if ($s1 === $s2) {
+        echo "Singleton works, both variables contain the same instance.";
+    } else {
+        echo "Singleton failed, variables contain different instances.";
+    }
+}
+
+clientCode();
